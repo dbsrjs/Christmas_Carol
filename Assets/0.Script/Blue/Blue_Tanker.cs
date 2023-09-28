0f65cb640cd23f5dd5ed86a82edfb75cf83d96e9
dbsrjs <dbsrjs1224@gmail.com>
2023-09-29 05:24:24 +0900

    9/29 Christmas_Carol

--- Assets/0.Script/Blue/Blue_Tanker.cs
+++ Assets/0.Script/Blue/Blue_Tanker.cs
@@ -4,18 +4,81 @@ using UnityEngine;
 
 public class Blue_Tanker : MonoBehaviour
 {
+    [SerializeField] private Red_Tanker monster;
     [SerializeField] private Animator animator;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    GameObject[] redObjects;
 
+    GameObject closestRedObject = null;
+    float minDistance = Mathf.Infinity;
+
+    float atkTime = 1.5f;    //공격 속도
+    private float atkTimer;
+
+    public int HP = 100; // public int HP { get; set; }
+
+    IEnumerator Start()
+    {
+        redObjects = GameObject.FindGameObjectsWithTag("Red");
+        yield return new WaitForSeconds(0.5f);
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(1, 0, 0) * 0.7f * Time.deltaTime;
-        animator.SetTrigger("doMove");
+        if (HP <= 0 || redObjects == null)   //플레이어가 없거나 죽었을 때
+            return;
+
+        if (redObjects != null)
+        {
+            foreach (GameObject redObject in redObjects)
+            {
+                if (redObject != null)
+                {
+                    float distance = Vector2.Distance(redObject.transform.position, transform.position);
+                    // 이제 distance 변수를 사용하여 필요한 작업을 수행할 수 있습니다.
+
+                    if (distance <= minDistance)  //거리가 1 이하라면
+                    {
+                        closestRedObject = redObject;
+                        minDistance = distance;
+
+                        atkTimer += Time.deltaTime;
+                        //공격
+                        if (atkTimer > atkTime)
+                        {
+                            atkTimer = 0;
+                            monster.Hit(40);
+                            animator.SetTrigger("doAttack");    //doAttack 애니메이션 실행
+                        }
+                    }
+
+                    else
+                    {
+                        if (closestRedObject != null)
+                        {
+                            animator.SetTrigger("doMove");    //doMove 애니메이션 실행
+                            float speed = 0.3f;
+                            Vector2 direction = (closestRedObject.transform.position - transform.position).normalized;
+                            transform.position += (Vector3)direction * speed * Time.deltaTime;
+                        }
+                    }
+                }                
+            }
+        }
+    }
+
+    public void Hit(int damage)    //damage = power(Monster) = 10;
+    {
+        Debug.Log("Blue Attack");
+        animator.SetTrigger("doHit");    //doHit 애니메이션 실행
+        HP -= damage;   //HP 감소
+
+        if (HP <= 0)    //죽었을 때
+        {
+            Destroy(GetComponent<Rigidbody2D>());   //Rigidbody2D 삭제
+            animator.SetTrigger("doDie");    //doDie 애니메이션 실행
+            //yield return new WaitForSeconds(2f);    //2초 후
+            Destroy(gameObject);    //몬스터 삭제
+        }
     }
 }
--- Assets/0.Script/Red/Red_Tanker.cs
+++ Assets/0.Script/Red/Red_Tanker.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class Red_Tanker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Blue_Tanker p;
+    [SerializeField] private Animator animator;
+
+    GameObject[] blueObjects;
+
+    GameObject closestRedObject = null;
+    float minDistance = Mathf.Infinity;
+
+    float atkTime = 1.6f;    //공격 속도
+
+    [HideInInspector] public int hp = 100;   //채력
+
+    private float atkTimer;
 
     // Update is called once per frame
+
+    IEnumerator Start()
+    {
+        blueObjects = GameObject.FindGameObjectsWithTag("Blue");
+        yield return new WaitForSeconds(0.5f);
+    }
     void Update()
     {
-        
+        if (p == null || hp <= 0 || blueObjects == null)   //플레이어가 없거나 죽었을 때
+            return;
+
+        if (blueObjects != null)
+        {
+            foreach (GameObject blueObject in blueObjects)
+            {
+                if (blueObject != null)
+                {
+                    float distance = Vector2.Distance(blueObject.transform.position, transform.position);
+                    // 이제 distance 변수를 사용하여 필요한 작업을 수행할 수 있습니다.
+
+                    if (distance <= minDistance)  //거리가 1 이하라면
+                    {
+                        closestRedObject = blueObject;
+                        minDistance = distance;
+
+                        atkTimer += Time.deltaTime;
+                        //공격
+                        if (atkTimer > atkTime)
+                        {
+                            atkTimer = 0;
+                            p.Hit(10);
+                        }
+                    }
+
+                    else
+                    {
+                        if (closestRedObject != null)
+                        {
+                            animator.SetTrigger("doMove");    //doMove 애니메이션 실행
+                            float speed = 0.3f;
+                            Vector2 direction = (closestRedObject.transform.position - transform.position).normalized;
+                            transform.position += (Vector3)direction * speed * Time.deltaTime;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void Hit(int damage)
+    {
+        Debug.Log("Red Attack");
+        hp -= damage;   //데미지에 비례해서 hp 감소
+        animator.SetTrigger("doHit");    //hit 애니메이션 실행
+
+        if (hp <= 0)    //죽으면
+        {
+            Destroy(GetComponent<Rigidbody2D>());   //Rigidbody2D 삭제
+            animator.SetTrigger("doDie");    //doDie 애니메이션 실행
+            //yield return new WaitForSeconds(2f);    //2초 후
+            Destroy(gameObject);    //몬스터 삭제
+        }
     }
 }
