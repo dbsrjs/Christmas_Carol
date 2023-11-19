eb403c923851776fa81a8ea72fa906605210e1ce
dbsrjs <dbsrjs1224@gmail.com>
2023-11-19 22:14:17 +0900

    11/17 Chrismas_Carol
    UI.num

--- Assets/0.Script/Ui/Ui.cs
+++ Assets/0.Script/Ui/Ui.cs
@@ -10,33 +10,28 @@ public class Ui : MonoBehaviour
 
     [SerializeField] private Text red_Text;
     [SerializeField] private Text red_Shadow;
-    private int red_num = 20;
 
     [SerializeField] private Text blue_Text;
     [SerializeField] private Text blue_Shadow;
-    private int blue_num = 20;
-
-    private int red_health = 200;
-    private int blue_health = 200;
 
     [SerializeField] private Slider cost_Bar;
     [SerializeField] private Text cost_text;
     [SerializeField] private Text cost_Shadow;
 
-    private GameManager gameManager;
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private Red_Base red_Base;
+    [SerializeField] private Blue_Base blue_Base;
     private float timer;
 
     void Start()
     {
-        gameManager = GetComponent<GameManager>();
-
         cost_Bar.value = 0;
 
-        red_Text.text = red_num.ToString();
-        red_Shadow.text = red_num.ToString();
+        red_Text.text = (red_Base.HP / 10).ToString();
+        red_Shadow.text = (red_Base.HP / 10).ToString();
 
-        blue_Text.text = blue_num.ToString();
-        blue_Shadow.text = blue_num.ToString();
+        blue_Text.text = (blue_Base.HP / 10).ToString();
+        blue_Shadow.text = (blue_Base.HP / 10).ToString();
 
         cost_text.text = cost_Bar.value.ToString();
         cost_Shadow.text = cost_Bar.value.ToString();
@@ -56,20 +51,16 @@ public class Ui : MonoBehaviour
     public void Red_Hit(int damage)
     {
         RectTransform rect_tran = red_Health.GetComponent<RectTransform>();
-        red_health -= damage;
-        red_num -= 1;
-        red_Text.text = red_num.ToString();
-        red_Shadow.text = red_num.ToString();
-        rect_tran.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, red_health);
+        red_Text.text = (red_Base.HP / 10).ToString();
+        red_Shadow.text = (red_Base.HP / 10).ToString();
+        rect_tran.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, red_Base.HP);
     }
 
     public void Blue_Hit(int damage)
     {
         RectTransform rect_tran = blue_Health.GetComponent<RectTransform>();
-        blue_health -= damage;
-        blue_num -= 1;
-        blue_Text.text = blue_num.ToString();
-        blue_Shadow.text = blue_num.ToString();
-        rect_tran.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, blue_health);
+        blue_Text.text = (blue_Base.HP / 10).ToString();
+        blue_Shadow.text = (blue_Base.HP / 10).ToString();
+        rect_tran.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, blue_Base.HP);
     }
 }
